fb3816a7e662a9484488eed5305429e83bf5e987
SnoxE <oicmyzs2000@gmail.com>
2023-10-08 12:32:10 +0200

    Add all the files

---
+++ src/components/MainNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="w-full text-sm">
+    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+      <div class="flex flex-nowrap h-full border-b border-solid border-mcl-orange mx-auto px-8">
+        <a class="flex h-full items-center text-xl text-">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">Developed by {{ author.firstname }}</h2>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'SnoxE Careers',
+      author: {
+        firstname: 'SnoxE'
+      },
+      url: 'https://careers.google.com'
+    }
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue, js, ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'mcl-orange': '#ff6d01',
+      },
+    },
+  },
+  plugins: [],
+}
