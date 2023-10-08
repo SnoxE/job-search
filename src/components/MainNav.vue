d3121ac6f358c9b96c883bd1e79fd201a6935af3
SnoxE <oicmyzs2000@gmail.com>
2023-10-08 13:24:00 +0200

    v-fro implemented

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,8 +2,15 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-mcl-orange mx-auto px-8">
-        <a class="flex h-full items-center text-xl text-">{{ company }}</a>
-        <h2 class="ml-8 flex h-full items-center">Developed by {{ author.firstname }}</h2>
+        <a :href="url" class="flex h-full items-center text-xl text-">{{ company }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="menuItem in menuItems" class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -18,7 +25,8 @@ export default {
       author: {
         firstname: 'SnoxE'
       },
-      url: 'https://careers.google.com'
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Location', 'Life @ SnoxE Corp', 'How we hire', 'Students', 'Jobs']
     }
   }
 }
