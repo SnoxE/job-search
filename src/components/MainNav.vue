e6e75312b279334ad7fb3a26d12e5ae54be844e0
SnoxE <oicmyzs2000@gmail.com>
2023-10-09 21:39:41 +0200

    mfs be centering now

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,13 +1,15 @@
 <template>
   <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white">
-      <div class="flex flex-nowrap h-full border-b border-solid border-mcl-orange mx-auto px-8">
-        <a :href="url" class="flex h-full items-center text-xl text-">{{ company }}</a>
+    <div class="fixed top-0 left-0 w-full min-h-16 bg-white">
+      <div
+        class="flex flex-nowrap h-full border-b border-solid border-mcl-orange mx-auto px-8 place-items-center"
+      >
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
 
         <nav class="ml-12 h-full">
-          <ul class="flex h-full list-none">
-            <li v-for="menuItem in menuItems" class="ml-9 h-full">
-              <a href="" class="flex h-full items-center py-2.5">{{ menuItem }}</a>
+          <ul class="flex h-full list-none items-center">
+            <li v-for="menuItem in menuItems" :key="menuItem" class="ml-9 h-full">
+              <a href="" class="flex h-full py-2.5">{{ menuItem }}</a>
             </li>
           </ul>
         </nav>
